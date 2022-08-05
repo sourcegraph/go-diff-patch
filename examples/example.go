03583810d453c38803a0f6d02fb89f2d727c769b
BolajiOlajide <25608335+BolajiOlajide@users.noreply.github.com>
2022-08-05 15:59:55 +0100

    add fixtures

---
+++ examples/example.go
@@ -0,0 +1,19 @@
+package example
+
+import (
+	"os"
+
+	godiffpatch "github.com/BolajiOlajide/go-diff-patch"
+)
+
+func main() {
+	patch := godiffpatch.GeneratePatch("test.txt", original, updated)
+	err := os.WriteFile("gitignore.patch", []byte(patch), 0644)
+	if err != nil {
+		panic(err)
+	}
+}
+
+const original = ``
+
+const updated = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
