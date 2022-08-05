83770d8d140c1e8199ada2425bd846f78339d08a
BolajiOlajide <25608335+BolajiOlajide@users.noreply.github.com>
2022-08-05 13:50:02 +0100

    generate correct patch for deleted LOC

--- internal/diff/unified.go
+++ internal/diff/unified.go
@@ -182,15 +182,16 @@ func (u Unified) Format(f fmt.State, r rune) {
 			}
 		}
 		fmt.Fprint(f, "@@")
+		fmt.Println(fromCount, toCount, "<===", hunk.ToLine)
 		if fromCount > 1 {
 			fmt.Fprintf(f, " -%d,%d", hunk.FromLine, fromCount)
 		} else {
-			fmt.Fprintf(f, " -%d", hunk.FromLine)
+			fmt.Fprintf(f, " -%d,%d", fromCount, fromCount)
 		}
 		if toCount > 1 {
 			fmt.Fprintf(f, " +%d,%d", hunk.ToLine, toCount)
 		} else {
-			fmt.Fprintf(f, " +%d", hunk.ToLine)
+			fmt.Fprintf(f, " +%d,%d", toCount, toCount)
 		}
 		fmt.Fprint(f, " @@\n")
 		for _, l := range hunk.Lines {
