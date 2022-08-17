80f204daacaffe19f9330977598cec2abc021923
Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>
2022-08-17 16:41:11 +0100

    Update main.go
    Co-authored-by: Randell Callahan <piszmogcode@gmail.com>

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func GeneratePatch(filename, originalContent, updatedContent string) string {
 	uri := span.URI(filename)
 	diffs, err := myers.ComputeEdits(uri, originalContent, updatedContent)
 	if err != nil {
-		// Ideally, this would never happen because the error returned here is always nil for
+		// An error can never be returned here. It is always nil. The returned error is to satisfy the interface defined in `internal/diff/diff.go#L24`.
 		// the diffs calculated using Eugene Myers algorithm.
 		// Error is returned to satisfy the interface defined in `internal/diff/diff.go#L24`
 		panic(err)
