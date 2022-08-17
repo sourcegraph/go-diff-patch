abed006a664dc37ae11ca81568cc145524e7e5a0
BolajiOlajide <25608335+BolajiOlajide@users.noreply.github.com>
2022-08-17 03:10:07 +0100

    update descriptions

--- main.go
+++ main.go
@@ -8,13 +8,15 @@ import (
 	"github.com/sourcegraph/go-diff-patch/internal/span"
 )
 
-// TODO: add description
+// GeneratePatch generates a unified diff that is git-compatible and highlights the difference
+// between originalContent and updatedContent.
 func GeneratePatch(filename, originalContent, updatedContent string) string {
 	uri := span.URI(filename)
 	diffs, err := myers.ComputeEdits(uri, originalContent, updatedContent)
 	if err != nil {
-		// Ideally, this would never happen because the error returned here is always nil.
-		// Error is returned to satisfy the interface defined in `diff/diff.go#L24`
+		// Ideally, this would never happen because the error returned here is always nil for
+		// the diffs calculated using Eugene Myers algorithm.
+		// Error is returned to satisfy the interface defined in `internal/diff/diff.go#L24`
 		panic(err)
 	}
 
